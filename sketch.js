c6bbbed656c0e5d9d825970abf627b74da2eb2bc
Amanda Yeh <amandayeh123@gmail.com>
2020-11-16 22:37:08 -0800

    changed background and ui

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+let cnv;
+let width;
+let height;
 let mainF;
 let video;
 let inp;
 let mainText;
+let snap;
 let enter;
 let firstInput;
+let domBackground;
+let domForeground;
+let reroll;
+let swap;
+let selectedColors;
+let previousColors;
+let cacheForeground;
+
+
+
+let backgroundColor;
+let foregroundColor;
 
 let vscale = 16;
 
@@ -12,19 +28,45 @@ let textArray = ["M", "O", "V", "E", "M", "E", "N", "T"];
 let textIndex;
 let previousFrame = 0;
 
+let colors = [
+  ["#FFE483", "#EC651A"],["#004466", "#2AEAA4"],["#FFFFFF", "#000000"],["#FF3981", "#3C4548"], ["#EEA748", "#0D55B4"],["#00042a", "#985fe4"], ["#93058c", "#b9fdbc"], ["#16005f", "#737cce"], ["#682061", "#9ae2a1"], ["#181b7a","#ede788"]
+]
+
+
 function setup() {
-  let cnv = createCanvas(1100, 800);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
+  if (windowWidth - 80 < 1000) {
+    width = windowWidth - 80;
+  } else {
+    width = 1000; 
+  }
+  height = width * 0.7;
+  console.log(width,height)
+
+  cnv = createCanvas(width, height);
+  // var x = (windowWidth - width) / 2;
+  // var y =  80;
+  // cnv.position(x, y);
+  backgroundColor = "#3C4548"; 
+  foregroundColor = "#FF3981";
+  
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(1100 / vscale, 800 / vscale);
   video.hide();
   inp = select("input");
-  enter = select(".return");
+  enter = select(".controls__input__return");
+  snap = select(".controls__snap");
+  document.documentElement.style.setProperty("--color-first", backgroundColor);
+  document.documentElement.style.setProperty("--color-second", foregroundColor);
+  document.getElementById("controls__color__swap__stroke").style.stroke = foregroundColor;
+  document.getElementById("controls__color__swap__fill").style.fill = foregroundColor;
+  reroll = select(".controls__color__reroll");
+  swap = select(".controls__color__swap")
   firstInput = true;
   textIndex = 0;
+  selectedColors = [];
+
+
 }
 
 function draw() {
@@ -36,6 +78,11 @@ function draw() {
     }
     enter.mousePressed(changeText);
   }
+  snap.mousePressed(download);
+
+  reroll.mousePressed(changeColor);
+  swap.mousePressed(swapColor);
+
 
   if ($("input").is(":focus")) {
     if (firstInput) {
@@ -46,7 +93,7 @@ function draw() {
     }
   }
 
-  background(0);
+  background(backgroundColor);
   video.loadPixels();
   loadPixels();
   for (let y = 0; y < video.height; y++) {
@@ -58,13 +105,13 @@ function draw() {
       let b = video.pixels[index + 2];
 
       let dark = 255 - (r + g + b) / 3;
-
+      
       if (dark > 150) {
-        fill(255);
+        fill(foregroundColor);
 
         textSize(map(dark, 150, 255, 1, vscale));
       } else {
-        fill(0);
+        fill(backgroundColor);
         textSize(0);
       }
 
@@ -82,4 +129,44 @@ function inputEvent() {
 
 function changeText() {
   textArray = mainText.split("");
+}
+
+function download() {
+  saveCanvas(cnv, 'Type Mirror', 'png')
+}
+
+function changeColor() {
+
+  previousColors = selectedColors;
+  
+  selectedColors = random(colors);
+  while (selectedColors == previousColors) {
+    selectedColors = random(colors);
+  }
+  let r = random(0,1);
+  if (Math.random() >= 0.5) {
+    foregroundColor = selectedColors[0];
+    backgroundColor = selectedColors[1];
+  } else {
+    foregroundColor = selectedColors[1];
+    backgroundColor = selectedColors[0];
+  }
+
+  document.getElementById("controls__color__swap__stroke").style.stroke = foregroundColor;
+  document.getElementById("controls__color__swap__fill").style.fill = foregroundColor;
+
+  document.documentElement.style.setProperty("--color-first", backgroundColor);
+  document.documentElement.style.setProperty("--color-second", foregroundColor);
+}
+
+function swapColor() {
+  cacheForeground = foregroundColor;
+  foregroundColor = backgroundColor;
+  backgroundColor = cacheForeground;
+
+  document.getElementById("controls__color__swap__stroke").style.stroke = foregroundColor;
+  document.getElementById("controls__color__swap__fill").style.fill = foregroundColor;
+
+  document.documentElement.style.setProperty("--color-first", backgroundColor);
+  document.documentElement.style.setProperty("--color-second", foregroundColor);
 }
